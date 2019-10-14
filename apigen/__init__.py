5bbd520deea84b9c6a814c29f3da93d360369005
dev <sabrinasahli@live.fr>
2019-10-14 10:52:26 +0000

    javadoc insert after http endpoint

--- apigen/__init__.py
+++ apigen/__init__.py
@@ -1,42 +1,54 @@
 import re
 import sys
+from collections import namedtuple
 
-REGEXPS = {
-    r'@Prefix\("([a-zA-Z/]+)".*': 'context[url_prefix]',
-    r'@(\w+)\("([a-zA-Z/:\?=]+)".*': r'# \1 {url_prefix}\2'
-}
+LineHandler = namedtuple("LineHandler", "name regexp replacement")
+
+LINE_HANDLERS = [
+    LineHandler('prefix', r'@Prefix\("([a-zA-Z/]+)".*', 'context[url_prefix]'),
+    LineHandler('http_endpoint', r'@(\w+)\("([a-zA-Z/:\?=]+)".*', r'## \1 {url_prefix}\2'),
+]
+
 
 CONTEXT_REGEXP = re.compile(r'context\[([a-z_]+)\]')
 
 
-def handle_line(rawline, context, regexp_dict):
-    line = rawline.strip('\n').strip()
+def handle_line(rawline, context, line_handlers):
+    line = rawline.strip()
     if line.startswith('/**'):
         context['javadoc'] = 'true'
-        return line[3:]
+        context['javadoc_lines'] = []
+        return []
 
     if context['javadoc'] == 'true':
         javadoc_line = line.replace('*', '').strip()
         if javadoc_line.startswith('@param'):
-            return javadoc_line.replace('@param', '**Parameter**')
-        if javadoc_line.startswith('@return'):
-            return javadoc_line.replace('@return', '**Return**')
-        if javadoc_line.endswith('/'):
+            context['javadoc_lines'].append(javadoc_line.replace('@param', '**Parameter**'))
+        elif javadoc_line.startswith('@return'):
+            context['javadoc_lines'].append(javadoc_line.replace('@return', '**Return**'))
+        elif javadoc_line.endswith('/'):
             context['javadoc'] = 'false'
-            return javadoc_line.strip('/')
         else:
-            return javadoc_line
+            context['javadoc_lines'].append(javadoc_line)
+        return []
 
-    for regexp_str, replacement in regexp_dict.items():
-        regexp = re.compile(regexp_str)
+    for line_handler in line_handlers:
+        regexp = re.compile(line_handler.regexp)
         if regexp.match(line):
-            if 'context' in replacement:
-                key_match = CONTEXT_REGEXP.match(replacement)
+            if 'context' in line_handler.replacement:
+                key_match = CONTEXT_REGEXP.match(line_handler.replacement)
                 value_match = regexp.match(line)
                 context[key_match.group(1)] = value_match.group(1)
-                return
+                return []
+            elif line_handler.name == 'http_endpoint':
+                lines = [regexp.sub(line_handler.replacement, line).format(**context)]
+                if 'javadoc_lines' in context:
+                    lines += context['javadoc_lines']
+                    del context['javadoc_lines']
+                return lines
             else:
-                return regexp.sub(replacement, line).format(**context)
+                return [regexp.sub(line_handler.replacement, line).format(**context)]
+    return []
 
 
 def generate_doc_for_file(filepath):
@@ -45,7 +57,7 @@ def generate_doc_for_file(filepath):
         doc_lines = []
         context = {'javadoc': 'false'}
         for line in file.readlines():
-            doc_lines.append(handle_line(line, context, REGEXPS))
+            doc_lines += handle_line(line, context, LINE_HANDLERS)
         return [l for l in doc_lines if l is not None]
 
 
--- apigen/test/test_apigen.py
+++ apigen/test/test_apigen.py
@@ -1,52 +1,61 @@
 from unittest import TestCase
 
-from apigen import handle_line
+from apigen import handle_line, LineHandler
 
 
 class TestApigen(TestCase):
     def test_handle_empty_doc(self):
         context = {'javadoc': 'false'}
-        self.assertIsNone(handle_line('a line', context, {r'not matching': r''}))
+        self.assertEqual([], handle_line('a line', context, [LineHandler('name', r'not matching', r'')]))
 
     def test_handle_line(self):
         context = {'javadoc': 'false'}
-        self.assertEqual('# Post /my/url', handle_line('@Post("/my/url")\n', context, {r'@(\w+)\("([a-z/]+)".*': r'# \1 \2'}))
+        self.assertEqual(['a line'], handle_line('a line', context, [LineHandler('name', r'(.*)', r'\1')]))
+
+    def test_handle_http_method(self):
+        context = {'javadoc': 'false'}
+        self.assertEqual(['# Post /my/url'], handle_line('@Post("/my/url")', context, [LineHandler('http_endpoint', r'@(\w+)\("([a-z/]+)".*', r'# \1 \2')]))
+
+    def test_handle_http_method_with_javadoc_lines(self):
+        context = {'javadoc': 'false','javadoc_lines': ['line 1', 'line 2']}
+        self.assertEqual(['# Post /my/url', 'line 1', 'line 2'], handle_line('@Post("/my/url")', context, [LineHandler('http_endpoint', r'@(\w+)\("([a-z/]+)".*', r'# \1 \2')]))
+        self.assertEqual({'javadoc': 'false'}, context)
 
     def test_handle_line_with_param(self):
         context = {'javadoc': 'false'}
-        self.assertEqual('# Get url/with/:param', handle_line('@Get("url/with/:param")\n',context, {r'@(\w+)\("([a-z/:]+)".*': r'# \1 \2'}))
+        self.assertEqual(['# Get url/with/:param'], handle_line('@Get("url/with/:param")', context, [LineHandler('http_endpoint',r'@(\w+)\("([a-z/:]+)".*', r'# \1 \2')]))
 
-    def test_handle_line_with_prefix(self):
+    def test_handle_line_with_context_property(self):
         context = {'javadoc': 'false',
                    'url_prefix': 'pre/fix/'}
-        self.assertEqual('# Get pre/fix/url', handle_line('@Get("url")\n', context, {r'@(\w+)\("([a-z/:]+)".*': r'# \1 {url_prefix}\2'}))
+        self.assertEqual(['# Get pre/fix/url'], handle_line('@Get("url")', context, [LineHandler('http_endpoint', r'@(\w+)\("([a-z/:]+)".*',r'# \1 {url_prefix}\2')]))
 
-    def test_handle_prefix_line(self):
+    def test_handle_line_save_into_context(self):
         context = {'javadoc': 'false'}
-        self.assertIsNone(handle_line('@Prefix("prefix")\n', context, {r'@Prefix\("([a-z/]+)".*': 'context[url_prefix]'}))
+        self.assertEqual([], handle_line('@Prefix("prefix")', context, [LineHandler('context_line', r'@Prefix\("([a-z/]+)".*', 'context[url_prefix]')]))
         self.assertEqual({'javadoc': 'false', 'url_prefix': 'prefix'}, context)
 
     def test_handle_line_javadoc_begin(self):
        context=dict()
-       self.assertEqual('first line comment', handle_line('/**first line comment \n', context, dict()))
-       self.assertEqual({'javadoc': 'true'}, context)
+       self.assertEqual([], handle_line('/**', context, list()))
+       self.assertEqual({'javadoc': 'true', 'javadoc_lines': []}, context)
 
     def test_handle_line_javadoc_end(self):
         context = {'javadoc': 'true'}
-        self.assertEqual('last line comment' , handle_line('last line comment*/ \n', context, dict()))
+        self.assertEqual([], handle_line('*/', context, list()))
         self.assertEqual({'javadoc': 'false'}, context)
 
     def test_handle_line_javadoc(self):
-        context = {'javadoc': 'true'}
-        self.assertEqual('this is javadoc comment', handle_line('* this is javadoc comment', context, dict()))
-        self.assertEqual({'javadoc': 'true'}, context)
+        context = {'javadoc': 'true', 'javadoc_lines': []}
+        self.assertEqual([], handle_line('* this is javadoc comment', context, list()))
+        self.assertEqual({'javadoc': 'true', 'javadoc_lines': ['this is javadoc comment']}, context)
 
     def test_handle_line_javadoc_parameter(self):
-        context = {'javadoc': 'true'}
-        self.assertEqual('**Parameter** name parameter name', handle_line('* @param name parameter name', context, dict()))
-        self.assertEqual({'javadoc': 'true'}, context)
+        context = {'javadoc': 'true', 'javadoc_lines': []}
+        self.assertEqual([], handle_line('* @param name parameter name', context, list()))
+        self.assertEqual({'javadoc': 'true', 'javadoc_lines': ['**Parameter** name parameter name']}, context)
 
     def test_handle_line_javadoc_return(self):
-        context = {'javadoc': 'true'}
-        self.assertEqual('**Return** object returned', handle_line('* @return object returned', context, dict()))
-        self.assertEqual({'javadoc': 'true'}, context)
+        context = {'javadoc': 'true', 'javadoc_lines': []}
+        self.assertEqual([], handle_line('* @return object returned', context, list()))
+        self.assertEqual({'javadoc': 'true', 'javadoc_lines': ['**Return** object returned']}, context)
